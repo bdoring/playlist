2f42138c0bbb415d0ba052ec62d67824f1bf63a3
bdoring <doringbarbara@gmail.com>
2017-09-16 16:22:36 -0700

    final version

--- playlistPageScript.js
+++ playlistPageScript.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
   $('select').material_select();
 
   $.get("http://playlistapi.surge.sh/playlistAPI", function(data){
+    //loads and adds the album covers to the DOM
     for (let i = 0; i < data.results.length; i++) {
       let coverDIV = document.createElement("div");
       $(coverDIV).css("backgroundImage", `url(./myAPIimages/${data.results[i].cover_art})`);
       $(coverDIV).addClass("cover-art-small z-depth-5 hoverable");
       $(".album-display").append(coverDIV);
 
+      //adds the album info to the lower half of the page
       $(coverDIV).click(() => {
         $(".add-tracks").empty();
         $(".track-list").empty();
@@ -19,18 +21,19 @@ $(document).ready(function(){
           <h5>Add tracks from:</h5>
           <p>${data.results[i].artist}: ${data.results[i].title}</p>
           `, myAlbumCover);
-
+        //loads the album tracks to the page
         for (let j = 0; j < data.results[i].songs.length; j++) {
           $(".track-list").append(
             `<p>
               <input type="checkbox" id= "${j}" />
               <label for="${j}">${data.results[i].songs[j].title} ${data.results[i].songs[j].length}</label>
             </p>`);
-
+          //adds selected tracks to the bin that will be submitted -
           $($("label")[j]).click(() => {
 
-            let id = `${data.results[i].id}${j}`;
+            let id = `${data.results[i].id}${j}`; //assigns an id to each track being added to the bin
 
+            //if the track has already been added, it gets removed
             if ($($("label")[j]).attr("class") === "clicked") {
               $($("label")[j]).attr("class", "unclicked");
               $("p").remove(`#${id}`);
@@ -43,10 +46,7 @@ $(document).ready(function(){
               }
             })
         }
-
-
-        })
-
+      })
     }
   });
 
